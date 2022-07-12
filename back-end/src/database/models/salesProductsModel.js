56d76f0d1333a8ee4fd9235c21a2660bb4f563b2
Priscila Silva <priscilacds_100@yahoo.com.br>
2022-07-12 17:05:12 -0300

    feat(SalesProductsModel): Implementação

---
+++ back-end/src/database/models/salesProductsModel.js
@@ -0,0 +1,26 @@
+export const modelSalesProducts = (sequelize, DataTypes) => {
+  const SalesProducts = sequelize.define('SalesProduct', { 
+    saleId: DataTypes.INTEGER,
+    productId: DataTypes.INTEGER,
+    quantity: DataTypes.INTEGER,
+  }, { 
+    timestamps: false,
+    underscored: true,
+  });
+
+  SalesProducts.associate = (models) => {
+    models.Sales.belongsToMany(models.Products, {
+      as: 'Products',
+      through: SalesProducts,
+      foreignKey: 'saleId',
+      otherKey: 'productId',
+    });
+    models.Products.belongsToMany(models.Sales, {
+      as: 'Sales',
+      through: SalesProducts,
+      foreignKey: 'productId',
+      otherKey: 'saleId',
+    });
+  };
+  return SalesProducts;
+};

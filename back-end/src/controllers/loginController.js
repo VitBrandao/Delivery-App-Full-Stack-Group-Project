714adcb7318a2bfa6111380e85f444055daab1aa
Priscila Silva <priscilacds_100@yahoo.com.br>
2022-07-13 15:12:40 -0300

    feat(Correções): LoginService e LoginController
    Implementações das validações do email e senha

--- back-end/src/controllers/loginController.js
+++ back-end/src/controllers/loginController.js
@@ -6,8 +6,10 @@ const loginController = async (req, res, _next) => {
   try {
     const loginInfos = await login(body);
 
-    if (loginInfos === null) return res.status(404).json({ message: 'Email or Password not valid' });
-
+    if (loginInfos === null) {
+      return res.status(404).json({ message: 'Email or Password not valid' });
+    }
+    
     const { email, name, role } = loginInfos;
     return res.status(200).json({ name, email, role, token: token(loginInfos.email) });
   } catch (err) {

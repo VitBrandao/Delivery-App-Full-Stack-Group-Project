c2ca74c9dc5802f1ac326848e1493d79c19d9809
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-07-13 18:37:02 -0300

    :rotating_light: fix: erros de linter

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -2,11 +2,9 @@ const express = require('express');
 const cors = require('cors');
 
 const loginRoutes = require('../routes/loginRoutes');
-
 const saleRoutes = require('../routes/salesRoutes');
 const saleProductRoutes = require('../routes/salesProductsRoutes');
 const registerRoutes = require('../routes/registerRoutes');
-
 
 const app = express();
 
@@ -14,6 +12,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/', loginRoutes);
+
 app.use('/', registerRoutes);
 
 app.use('/', saleRoutes);

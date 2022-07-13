3e5927517cc033aa99817a33b254343542176f8b
vitor <vitorsbsilva@gmail.com>
2022-07-13 15:04:38 -0300

    refactor: comment login middleware

--- back-end/src/middlewares/loginAndRegisterMiddlewares.js
+++ back-end/src/middlewares/loginAndRegisterMiddlewares.js
@@ -1,13 +1,13 @@
-const { user } = require('../database/models');
+// const { user } = require('../database/models');
 
-const verifyEmailInDB = async (req, res, next) => {
-  const { email } = req.body;
-  const allUsers = await user.findAll();
-  const findByEmail = allUsers.find((userEmail) => userEmail.email === email);
-  if (!findByEmail) return res.status(404).json({ message: 'Not found' });
-  next();
-};
+// const verifyEmailInDB = async (req, res, next) => {
+//   const { email } = req.body;
+//   const allUsers = await user.findAll();
+//   const findByEmail = allUsers.find((userEmail) => userEmail.email === email);
+//   if (!findByEmail) return res.status(404).json({ message: 'Not found' });
+//   next();
+// };
 
-module.exports = {
-  verifyEmailInDB,
-};
+// module.exports = {
+//   verifyEmailInDB,
+// };

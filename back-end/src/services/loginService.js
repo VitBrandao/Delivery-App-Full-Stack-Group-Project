a61bf62a88c022c04709e7249e2cf2ab9f716738
vitor <vitorsbsilva@gmail.com>
2022-07-13 15:04:50 -0300

    refactor: Login Services

--- back-end/src/services/loginService.js
+++ back-end/src/services/loginService.js
@@ -3,9 +3,10 @@ const { user } = require('../database/models');
 
 const login = async ({ email, password }) => {
   const hashPassword = md5(password);
-  const userLogin = await user.findOne({ where: { email } });
-  // if (!userLogin) return { message: '404 - Not found' };
-  if (userLogin.password !== hashPassword) return null;
+  const userLogin = await user.findOne({ where: { email, password: hashPassword } });
+  
+  if (!userLogin) return null;
+  
   return userLogin;
 };
 

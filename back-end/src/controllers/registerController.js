0c8a317b3fd5211a375a85db3f56fbf0315648bc
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-07-13 18:54:56 -0300

    :sparkles: feat: Endpoint POST /admin/manage
    responsável por criar um usuário através do fluxo de admin

--- back-end/src/controllers/registerController.js
+++ back-end/src/controllers/registerController.js
@@ -1,4 +1,4 @@
-const register = require('../services/registerService');
+const { register, registerByAdmin } = require('../services/registerService');
 const token = require('../services/generateToken');
 
 const registerController = async (req, res, _next) => {
@@ -17,4 +17,20 @@ const registerController = async (req, res, _next) => {
   }
 };
 
-module.exports = registerController;
+const registerByAdminController = async (req, res, _next) => {
+  const { body } = req;
+  try {
+    const registerInfos = await registerByAdmin(body);
+
+    if (registerInfos === null) {
+      return res.status(409).json({ message: 'User already exists' });
+    }
+
+    const { email, name, role } = registerInfos;
+    return res.status(201).json({ name, email, role });
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+};
+
+module.exports = { registerController, registerByAdminController };

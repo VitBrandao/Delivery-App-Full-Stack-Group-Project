ced6a3a5f11b417fa2c1bd1a69c13fb2260662b3
vitor <vitorsbsilva@gmail.com>
2022-07-13 15:05:18 -0300

    fix: comment error middleware in Routes

--- back-end/src/routes/loginRoutes.js
+++ back-end/src/routes/loginRoutes.js
@@ -1,9 +1,9 @@
 const { Router } = require('express');
 const loginController = require('../controllers/loginController');
-const { verifyEmailInDB } = require('../middlewares/loginAndRegisterMiddlewares');
+// const { verifyEmailInDB } = require('../middlewares/loginAndRegisterMiddlewares');
 
 const router = Router();
 
-router.post('/login', verifyEmailInDB, loginController);
+router.post('/login', loginController);
 
 module.exports = router;

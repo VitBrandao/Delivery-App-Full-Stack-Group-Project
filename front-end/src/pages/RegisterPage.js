4bc90d4d75c19db66981c209383b4441b0d9f93d
Rafaela Camargos <rafaela.camargos.ribeiro@gmail.com>
2022-07-15 17:38:22 -0300

    Feat: Front-End - Tela de Registro: Implementação dos campos de input name, email, e password; Implementação do botão cadastrar; Implementação da função handleInput responsável por capturar  os valores de input e setar esses valores para o estado.

--- front-end/src/pages/RegisterPage.js
+++ front-end/src/pages/RegisterPage.js
@@ -1,9 +1,60 @@
 import React from 'react';
 
 function RegisterPage() {
+  const [register, setRegister] = React.useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  function handleInput({ target: { name, value } }) {
+    setRegister({ ...register, [name]: value });
+  }
+
   return (
     <div>
-      <h1>Register Page</h1>
+      <h1>Cadastro</h1>
+      <form>
+        <label htmlFor="input-name">
+          Nome
+          <input
+            data-testid="common_register__input-name"
+            type="text"
+            name="name"
+            placeholder="Seu nome"
+            value={ register.name }
+            onChange={ handleInput }
+          />
+        </label>
+        <label htmlFor="input-email">
+          Email
+          <input
+            data-testid="common_register__input-email"
+            type="text"
+            name="email"
+            placeholder="seu-email@site.com.br"
+            value={ register.email }
+            onChange={ handleInput }
+          />
+        </label>
+        <label htmlFor="input-password">
+          Senha
+          <input
+            data-testid="common_register__input-password"
+            type="password"
+            name="password"
+            placeholder="*******"
+            value={ register.password }
+            onChange={ handleInput }
+          />
+        </label>
+      </form>
+      <button
+        data-testid="common_register__button-register"
+        type="button"
+      >
+        CADASTRAR
+      </button>
     </div>
   );
 }

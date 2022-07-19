4f524a4a1554ca4e90be2368619bf7dfc146f1d3
hector <hectoreddbenitez@gmail.com>
2022-07-19 17:11:57 -0300

    feat: fluxo do cliente -  Costumer Product Page

---
+++ front-end/src/pages/CustomerProductsPage.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import Header from "../components/Header";
+import { getItemLocalStorage } from "../helpers/localStorage";
+import Card from "../components/Card";
+import { getAll } from "../helpers/api/requests";
+
+function CustomerProductsPage() {
+  const [dataUser, setDataUser] = useState({
+    name: "",
+    email: "",
+    role: "",
+    token: "",
+  });
+  const [products, setProducts] = useState([]);
+
+  const catchDataUser = () => {
+    const data = getItemLocalStorage();
+    setDataUser(data);
+  };
+
+  const allProducts = async () => {
+    const response = await getAll('products');
+    console.log(response);
+    setProducts(response);
+  }
+
+  useEffect(() => {
+    catchDataUser();
+    allProducts();
+  }, []);
+
+  return (
+    <div>
+      <Header
+        buttonOne="Produtos"
+        buttonTwo="Meus Pedidos"
+        role={dataUser.role}
+        name={dataUser.name}
+      />
+
+
+    </div>
+  );
+}
+
+export default CustomerProductsPage;

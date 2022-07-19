08caadcd7391229cf9f67e6069412bd5ccef86c5
Priscila Silva <priscilacds_100@yahoo.com.br>
2022-07-18 21:06:54 -0300

    feat(Correção (Backend)): Validação do token

--- back-end/src/routes/registerRoutes.js
+++ back-end/src/routes/registerRoutes.js
@@ -3,11 +3,12 @@ const {
   registerController, 
   registerByAdminController,
 } = require('../controllers/registerController');
+const authorizationToken = require('../middlewares/auth/authorizationToken');
 
 const router = Router();
 
 router.post('/register', registerController);
 
-router.post('/admin/manage', registerByAdminController);
+router.post('/admin/manage', authorizationToken, registerByAdminController);
 
 module.exports = router;
--- back-end/src/services/generateToken.js
+++ back-end/src/services/generateToken.js
@@ -1,10 +1,8 @@
 const jwt = require('jsonwebtoken');
 const { readFileSync } = require('fs');
-const path = require('path');
 
 const generateToken = (email) => {
-  const keyPath = path.resolve(__dirname, '..', '..', 'jwt.evaluation.key');
-  const secretKey = readFileSync(keyPath, 'utf-8');
+  const secretKey = readFileSync('jwt.evaluation.key', 'utf-8');
 
   const jwtConfig = {
     expiresIn: '7d',

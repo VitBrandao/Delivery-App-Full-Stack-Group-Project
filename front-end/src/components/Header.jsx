08de03642e1d14d735b24520a662dd97951c991e
vitor <vitorsbsilva@gmail.com>
2022-07-20 19:32:35 -0300

    fix: Header. Adiciona data-testid dinâmico e renderização do preço total

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { removeItemLocalStorage } from '../helpers/localStorage';
 
-function Header({ buttonOne, buttonTwo, role, name }) {
+function Header({ buttonOne, buttonTwo, role, name, testId }) {
   return (
-    <header>
+    <header data-testid={ testId }>
       <button
         type="button"
         data-testid="customer_products__element-navbar-link-orders"
       >
         { buttonOne }
       </button>
+
       {
         role === 'customer' && (
           <button
@@ -21,12 +23,15 @@ function Header({ buttonOne, buttonTwo, role, name }) {
           </button>
         )
       }
+
       <span data-testid="customer_products__element-navbar-user-full-name">
         { name }
       </span>
+
       <Link
-        to="/login"
+        href="/login"
         data-testid="customer_products__element-navbar-link-logout"
+        onClick={ () => removeItemLocalStorage('user') }
       >
         Sair
       </Link>
@@ -37,11 +42,13 @@ function Header({ buttonOne, buttonTwo, role, name }) {
 Header.defaultProps = {
   buttonTwo: '',
 };
+
 Header.propTypes = {
   buttonOne: PropTypes.string.isRequired,
   buttonTwo: PropTypes.string,
   role: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
+  testId: PropTypes.string.isRequired,
 };
 
 export default Header;

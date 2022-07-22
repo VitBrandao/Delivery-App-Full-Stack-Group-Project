ce45d30107bfafa5c912793485cc2495d6306be3
Rafaela Camargos <rafaela.camargos.ribeiro@gmail.com>
2022-07-22 17:48:15 -0300

    Feat: Front-end - Checkout Orders Page - implementação da função que salva os dados do produto e do vendedor e redireciona para a pagina de detalhes do pedido.

--- front-end/src/pages/CustomerCheckoutPage.js
+++ front-end/src/pages/CustomerCheckoutPage.js
@@ -1,16 +1,45 @@
 import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import { getItemLocalStorage, setItemLocalStorage } from '../helpers/localStorage';
 import CheckoutOrders from '../components/CheckoutOrders';
-import DetailsDelivery from '../components/DetailsDelivery';
-import { fetchPost } from '../helpers/api/requests';
+import { fetchPost, getAll } from '../helpers/api/requests';
 
 function CustomerCheckoutPage() {
+  const history = useHistory();
+
   const [user, setUser] = useState({
     name: '', email: '', role: '', token: '',
   });
 
+  const [sellers, setSellers] = useState([]);
+
   const [products, setProducts] = useState([]);
+
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  const calculateTotalCard = () => {
+    const total = products
+      .reduce((prev, current) => prev + parseFloat(current.subTotal), 0);
+    setTotalPrice(total);
+  };
+
+  useEffect(() => {
+    calculateTotalCard();
+  }, [products]);
+
+  const [deliveryInfo, setDeliveryInfo] = useState({
+    sellerName: '',
+    sellerId: '',
+    address: '',
+    number: '',
+  });
+
+  const getSellers = async () => {
+    const response = await getAll('users');
+    const filterSeller = response.filter((seller) => (seller.role === 'seller'));
+    setSellers(filterSeller);
+  };
 
   const catchProducts = () => {
     const data = getItemLocalStorage('carrinho');
@@ -22,24 +51,41 @@ function CustomerCheckoutPage() {
     setUser(data);
   };
 
-  // refatorar fetch com o userId
-
   const registerNewSale = async () => {
+    const arrayProducts = [];
+
+    products.map((product) => (
+      arrayProducts.push({ productId: product.id, quantity: product.quantity })
+    ));
+
     setItemLocalStorage('carrinho', products);
-    await fetchPost({
-      userId,
-      sellerId,
+    const response = await fetchPost({
+      userId: user.id,
+      sellerId: deliveryInfo.sellerId,
       totalPrice,
-      deliveryAddress,
-      deliveryNumber,
-      status: 'pendente',
-      products,
+      deliveryAddress: deliveryInfo.address,
+      deliveryNumber: deliveryInfo.number,
+      status: 'Pendente',
+      products: arrayProducts,
     }, 'sales');
+
+    history.push(`customer/orders/${response}`);
+  };
+
+  const handleInput = ({ target }) => {
+    if (target.name === 'sellerName') {
+      const infoSeller = sellers.find((seller) => seller.name === target.value);
+      setDeliveryInfo({
+        ...deliveryInfo, [target.name]: target.value, sellerId: infoSeller.id });
+    } else {
+      setDeliveryInfo({ ...deliveryInfo, [target.name]: target.value });
+    }
   };
 
   useEffect(() => {
     catchDataUser();
     catchProducts();
+    getSellers();
   }, []);
 
   return (
@@ -54,8 +100,53 @@ function CustomerCheckoutPage() {
       <CheckoutOrders
         products={ products }
         setProducts={ setProducts }
+        totalPrice={ totalPrice }
       />
-      <DetailsDelivery />
+      <h1>Detalhes e Endereço para Entrega</h1>
+      <form>
+        <label htmlFor="select-seller">
+          P.Vendedora Responsável
+          <select
+            data-testid="customer_checkout__select-seller"
+            onChange={ handleInput }
+            value={ deliveryInfo.sellerName }
+            name="sellerName"
+          >
+            <option>
+              selecione um vendedor
+            </option>
+            {
+              sellers.map((seller) => (
+                <option
+                  key={ seller.id }
+                >
+                  {seller.name}
+                </option>
+              ))
+            }
+          </select>
+        </label>
+        <label htmlFor="input-address">
+          Endereço
+          <input
+            data-testid="customer_checkout__input-address"
+            type="text"
+            onChange={ handleInput }
+            name="address"
+            value={ deliveryInfo.address }
+          />
+        </label>
+        <label htmlFor="input-address">
+          Número
+          <input
+            data-testid="customer_checkout__input-addressNumber"
+            type="text"
+            onChange={ handleInput }
+            value={ deliveryInfo.number }
+            name="number"
+          />
+        </label>
+      </form>
       <button
         data-testid="customer_checkout__button-submit-order"
         type="button"

dd53750df55b1fd891bd570835e33bb02a611754
Rafaela Camargos <rafaela.camargos.ribeiro@gmail.com>
2022-07-22 19:36:33 -0300

    Fix: BackEnd - altera o nome da tabela para camel case

--- back-end/src/database/models/salesProductsModel.js
+++ back-end/src/database/models/salesProductsModel.js
@@ -1,17 +1,17 @@
 const salesProductsModel = (sequelize, DataTypes) => {
   const SalesProducts = sequelize.define('salesProduct', { 
     saleId: DataTypes.INTEGER, productId: DataTypes.INTEGER, quantity: DataTypes.INTEGER,
-  }, { timestamps: false, underscored: true });
+  }, { timestamps: false, underscored: true, tableName: 'salesProducts' });
 
   SalesProducts.associate = (models) => {
     models.sale.belongsToMany(models.product, {
-      as: 'product',
+      as: 'products',
       through: SalesProducts,
       foreignKey: 'saleId',
       otherKey: 'productId',
     });
     models.product.belongsToMany(models.sale, {
-      as: 'sale',
+      as: 'sales',
       through: SalesProducts,
       foreignKey: 'productId',
       otherKey: 'saleId',

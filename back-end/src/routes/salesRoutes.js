214f118c48dce3615e8ce649d6e60f89480edf98
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-07-22 19:57:09 -0300

    :recycle: refactor: POST /sales
    Adiciona validação de token na rota

--- back-end/src/routes/salesRoutes.js
+++ back-end/src/routes/salesRoutes.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
 const salesController = require('../controllers/salesController');
+const authorizationToken = require('../middlewares/auth/authorizationToken');
 
 const router = Router();
 
-router.post('/sales', salesController.createSale);
+router.post('/sales', authorizationToken, salesController.createSale);
 router.post('/seller/orders', salesController.getSalesBySeller);
 router.put('/sales/:id', salesController.updateStatusSale);
 
